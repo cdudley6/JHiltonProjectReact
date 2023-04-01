461fe00fb7d028e0ad756c8a9c37acbee34dd9a2
Carson Dudley <cdudley6@byu.edu>
2023-03-31 22:53:03 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TopBanner from './blah';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBanner />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/blah.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TopBanner() {
+  return (
+    <React.Fragment>
+      <img src="./logo192.png" alt="companyLogo" />
+      <h1>Joel Hilton's collection</h1>
+    </React.Fragment>
+  );
+}
+
+export default TopBanner;
